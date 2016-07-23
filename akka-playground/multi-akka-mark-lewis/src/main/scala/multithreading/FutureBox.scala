d361e745f48d49945674c972d945b042c10c50d6
Orest Ivasiv <halyph@gmail.com>
2016-07-23 18:15:44 +0300

    [akka]  Future onCompleate sample

--- akka-playground/multi-akka-mark-lewis/src/main/scala/multithreading/FutureBox.scala
+++ akka-playground/multi-akka-mark-lewis/src/main/scala/multithreading/FutureBox.scala
@@ -3,8 +3,9 @@ package multithreading
 import io.StdIn._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
-object FutureBox extends App{
+object FutureBox extends App {
 
   println("This is 1st")
   val f = Future {
@@ -12,6 +13,17 @@ object FutureBox extends App{
   }
   Thread.sleep(1)
   println("This is Last")
+
+
+  val f2 = Future {
+    for(i <- 1 to 30 ) yield ParallelCollect.fib(i)
+  }
+
+  f2.onComplete {
+    case Success(n) => println(n)
+    case Failure(ex) => println("something went wrong " + ex)
+  }
+
 
   readLine()
 }
