5281b184829b2f6cdc7919f47435426431f35e50
Orest Ivasiv <halyph@gmail.com>
2016-07-24 17:46:08 +0300

    [akka] Actors URLs

--- akka-playground/multi-akka-mark-lewis/src/main/scala/akkasample/HierarchyExample.scala
+++ akka-playground/multi-akka-mark-lewis/src/main/scala/akkasample/HierarchyExample.scala
@@ -6,38 +6,39 @@ import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 object HierarchyExample extends App {
 
   case object CreateChild
-  case object SignalChildren
-  case object PrintSignal
+  case class SignalChildren(order: Int)
+  case class PrintSignal(order: Int)
 
   class ParentActor extends Actor {
     private var number = 0
-    private val children = collection.mutable.Buffer[ActorRef]()
-
     override def receive: Receive = {
       case CreateChild =>
-        children += context.actorOf(Props[ChildActor], "child" + number)
+        context.actorOf(Props[ChildActor], "child" + number)
         number += 1
-      case SignalChildren =>
-        children.foreach( _ ! PrintSignal)
+      case SignalChildren(n) =>
+        context.children.foreach( _ ! PrintSignal(n))
     }
   }
 
   class ChildActor extends Actor {
     override def receive = {
-      case PrintSignal => println(self)
+      case PrintSignal(n) => println(n + " " + self)
     }
   }
 
   val system = ActorSystem("HierarchySystem")
   val actor = system.actorOf(Props[ParentActor], "Parent1")
+  val actor2 = system.actorOf(Props[ParentActor], "Parent2")
 
   actor ! CreateChild
-  actor ! SignalChildren
+  actor ! SignalChildren(1)
   actor ! CreateChild
   actor ! CreateChild
-  actor ! SignalChildren
+  actor ! SignalChildren(2)
 
-
+  actor2 ! CreateChild
+  val child0 = system.actorSelection("akka://HierarchySystem/user/Parent2/child0")
+  child0 ! PrintSignal(3)
 
   Thread.sleep(2000)
   system.terminate()
