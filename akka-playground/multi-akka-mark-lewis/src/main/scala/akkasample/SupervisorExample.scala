3f513293420c33e6df654430a446f54ec66faa06
Orest Ivasiv <halyph@gmail.com>
2016-07-24 18:01:37 +0300

    [akka] OneForOne Supervision strategy

---
+++ akka-playground/multi-akka-mark-lewis/src/main/scala/akkasample/SupervisorExample.scala
@@ -0,0 +1,55 @@
+package akkasample
+
+import akka.actor.SupervisorStrategy.{Restart, Resume}
+import akka.actor.{Actor, ActorSystem, OneForOneStrategy, Props, SupervisorStrategy}
+import akkasample.HierarchyExample.{PrintSignal, SignalChildren}
+
+object SupervisorExample extends App {
+
+  case object CreateChild
+  case class SignalChildren(order: Int)
+  case class PrintSignal(order: Int)
+  case class DivideNumbers(n: Int, d: Int)
+  case object BadStuff
+
+
+  class ParentActor extends Actor {
+    private var number = 0
+    override def receive: Receive = {
+      case CreateChild =>
+        context.actorOf(Props[ChildActor], "child" + number)
+        number += 1
+      case SignalChildren(n) =>
+        context.children.foreach( _ ! PrintSignal(n))
+    }
+
+    override def supervisorStrategy = OneForOneStrategy(loggingEnabled = false)   {
+      case ae: ArithmeticException => Resume
+      case _: Exception => Restart
+    }
+  }
+
+  class ChildActor extends Actor {
+    override def receive = {
+      case PrintSignal(n) => println(n + " " + self)
+      case DivideNumbers(n, d) => println(n/d)
+      case BadStuff => throw new RuntimeException("Stuff happened")
+    }
+  }
+
+  val system = ActorSystem("HierarchySystem")
+  val actor = system.actorOf(Props[ParentActor], "Parent1")
+  val actor2 = system.actorOf(Props[ParentActor], "Parent2")
+
+  actor ! CreateChild
+  actor ! CreateChild
+
+  val child0 = system.actorSelection("akka://HierarchySystem/user/Parent1/child0")
+  child0 ! DivideNumbers(4,0)
+  child0 ! DivideNumbers(4,2)
+  child0 ! DivideNumbers(10,2)
+  child0 ! BadStuff
+
+  Thread.sleep(2000)
+  system.terminate()
+}
